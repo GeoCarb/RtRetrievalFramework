5af835f1107676cae1a9044bedf6a87557aa0e53
Greg McGarragh <g.mcgarragh@physics.ox.ac.uk>
2019-05-03 12:41:16 -0600

    Refactor the first experimental application of the Stokes coefficients as polynomials in commit c45d768 and use the central wavelengths from the L1B input.

--- lib/Interface/radiative_transfer_fixed_stokes_coefficient.cc
+++ lib/Interface/radiative_transfer_fixed_stokes_coefficient.cc
@@ -9,12 +9,12 @@ static const int max_num_stokes = 4;
 
 // See base class for description.
 Spectrum RadiativeTransferFixedStokesCoefficient::reflectance
-(const SpectralDomain& Spec_domain, int Spec_index, bool Skip_jacobian) const
+  (const SpectralDomain& Spec_domain, int Spec_index, bool Skip_jacobian) const
 {
-  firstIndex i1; secondIndex i2; thirdIndex i3;
+  firstIndex i1; secondIndex i2; thirdIndex i3; fourthIndex i4;
   range_check(Spec_index, 0, number_spectrometer());
   ArrayAd<double, 2> stk;
-  if(Skip_jacobian)
+  if (Skip_jacobian)
     stk.reference(ArrayAd<double, 2>(stokes(Spec_domain, Spec_index)));
   else
     stk.reference(stokes_and_jacobian(Spec_domain, Spec_index));
@@ -29,7 +29,8 @@ Spectrum RadiativeTransferFixedStokesCoefficient::reflectance
       Array<double, 1> stoke_val(stk.value()(Range::all(), stokes_idx));
       Array<double, 2> stoke_jac(stk.jacobian()(Range::all(), stokes_idx, Range::all()));
       SpectralRange stoke_range(ArrayAd<double, 1>(stoke_val, stoke_jac), units::inv_sr);
-      boost::shared_ptr<NamedSpectrum> stoke_ptr(new NamedSpectrum(Spec_domain, stoke_range, "stokes_" + std::string(stoke_names[stokes_idx]), Spec_index));
+      boost::shared_ptr<NamedSpectrum> stoke_ptr(new NamedSpectrum(Spec_domain, stoke_range,
+        "stokes_" + std::string(stoke_names[stokes_idx]), Spec_index));
       stoke_spectrums.push_back(stoke_ptr);
     }
     const_cast<RadiativeTransferFixedStokesCoefficient *>(this)->notify_update_do(stoke_spectrums);
@@ -37,49 +38,89 @@ Spectrum RadiativeTransferFixedStokesCoefficient::reflectance
 
   ArrayAd<double, 1> res(stk.rows(), stk.number_variable());
 
-  Array<double, 2> stokes_coef_sub(stokes_coef->stokes_coefficient().value()(Spec_index, Range(0, number_stokes() - 1), Range::all()));
-/*
-  res.value() = sum(stk.value()(i1, i2) * stokes_coef_sub(i2), i2);
-  if(!res.is_constant()) {
-    if(!stokes_coef->stokes_coefficient().is_constant()) {
-      Array<double, 2> stokes_coef_jac_sub(stokes_coef->stokes_coefficient().jacobian()(Spec_index, Range(0, number_stokes() - 1), Range::all()));
-      res.jacobian() =
-	sum(stk.jacobian()(i1, i3, i2) * stokes_coef_sub(i3), i3) +
-	sum(stk.value()(i1, i3) * stokes_coef_jac_sub(i3, i2), i3);
-    } else
-      res.jacobian() =
-	sum(stk.jacobian()(i1, i3, i2) * stokes_coef_sub(i3), i3);
-  } else if(!stokes_coef->stokes_coefficient().is_constant()) {
-      Array<double, 2> stokes_coef_jac_sub(stokes_coef->stokes_coefficient().jacobian()(Spec_index, Range(0, number_stokes() - 1), Range::all()));
-      res.jacobian() =
-	sum(stk.value()(i1, i3) * stokes_coef_jac_sub(i3, i2), i3);
+  Array<double, 2> stokes_coef_sub(stokes_coef->stokes_coefficient().value()
+    (Spec_index, Range(0, number_stokes() - 1), Range::all()));
+
+  Array<double, 1> delta_wl(1.0e3*Spec_domain.wavelength() -
+                   stokes_coef->stokes_coefficient_central_wl().value()(Spec_index));
+
+  Array<double, 2> factor(delta_wl.rows(), stokes_coef_sub.cols());
+
+  int use_old = 0;
+if (use_old) {
+  for (int i = 0; i < factor.cols(); ++i)
+    factor(Range::all(), i) = pow(delta_wl(Range::all()), (double) i);
+} else {
+  factor = pow(delta_wl(i1), i2);
+}
+if (use_old) {
+  res.value() = 0.;
+
+  for (int i = 0; i < stk.value().rows(); ++i) {
+    for (int j = 0; j < stokes_coef_sub.rows(); ++j) {
+      for (int k = 0; k < stokes_coef_sub.cols(); ++k)
+        res.value()(i) += stk.value()(i,j) * stokes_coef_sub(j,k) * factor(i,k);
+    }
   }
-*/
-/*
-  double stokes_coeff_central_wl[4] = {0., 0., 0., 0.};
-*/
-  double stokes_coeff_central_wl[4] = {7.650e+02, 1.606e+03, 2.065e+03, 2.323e+03};
+} else {
+  res.value() = sum(sum(stk.value()(i1, i3) * stokes_coef_sub(i3, i2), i3) *
+                    factor(i1, i2), i2);
+}
+if (use_old) {
+  res.jacobian() = 0.;
 
-  Array<double, 1> wl(Spec_domain.wavelength()(Range::all()));
-  Array<double, 1> HV(Spec_domain.wavelength()(Range::all()));
-  Array<double, 1> delta_wl(Spec_domain.wavelength()(Range::all()));
-/*
-  delta_wl =       wl - stokes_coeff_central_wl[Spec_index];
-*/
-  delta_wl = 1.0e3*wl - stokes_coeff_central_wl[Spec_index];
+  if (! res.is_constant()) {
+    if (! stokes_coef->stokes_coefficient().is_constant()) {
+      Array<double, 2> stokes_coef_jac_sub(stokes_coef->stokes_coefficient().jacobian()
+        (Spec_index, Range(0, number_stokes() - 1), Range::all()));
 
-  ArrayAd<double, 1> myres(res.copy());
+      for (int i = 0; i < stk.value().rows(); ++i) {
+        for (int j = 0; j < stokes_coef_sub.rows(); ++j) {
+          for (int k = 0; k < stokes_coef_sub.cols(); ++k)
+            res.jacobian()(i,Range::all()) +=
+              (stk.jacobian()(i,j,Range::all()) * stokes_coef_sub(j,k) +
+               stk.value()(i, j) * stokes_coef_jac_sub(j,k,Range::all())) * factor(i,k);
+        }
+      }
+    } else {
+      for (int i = 0; i < stk.value().rows(); ++i) {
+        for (int j = 0; j < stokes_coef_sub.rows(); ++j) {
+          for (int k = 0; k < stokes_coef_sub.cols(); ++k)
+            res.jacobian()(i,Range::all()) +=
+              stk.jacobian()(i,j,Range::all()) * stokes_coef_sub(j,k) * factor(i,k);
+        }
+      }
+    }
+  } else if (! stokes_coef->stokes_coefficient().is_constant()) {
+    Array<double, 2> stokes_coef_jac_sub(stokes_coef->stokes_coefficient().jacobian()
+      (Spec_index, Range(0, number_stokes() - 1), Range::all()));
 
-  for (int jj = 0; jj < stk.value().rows(); jj++) {
-    myres.value()(jj) = stk.value()(jj,0) * (stokes_coef_sub(0,0) + stokes_coef_sub(0,1) * delta_wl(jj)) +
-                        stk.value()(jj,1) * (stokes_coef_sub(1,0) + stokes_coef_sub(1,1) * delta_wl(jj)) +
-                        stk.value()(jj,2) * (stokes_coef_sub(2,0) + stokes_coef_sub(2,1) * delta_wl(jj));
-
-    myres.jacobian()(jj,Range::all()) =
-                        stk.jacobian()(jj,0,Range::all()) * (stokes_coef_sub(0,0) + stokes_coef_sub(0,1) * delta_wl(jj)) +
-                        stk.jacobian()(jj,1,Range::all()) * (stokes_coef_sub(1,0) + stokes_coef_sub(1,1) * delta_wl(jj)) +
-                        stk.jacobian()(jj,2,Range::all()) * (stokes_coef_sub(2,0) + stokes_coef_sub(2,1) * delta_wl(jj));
+    for (int i = 0; i < stk.value().rows(); ++i) {
+      for (int j = 0; j < stokes_coef_sub.rows(); ++j) {
+        for (int k = 0; k < stokes_coef_sub.cols(); ++k)
+          res.jacobian()(i,Range::all()) +=
+            stk.value()(i, j) * stokes_coef_jac_sub(j,k,Range::all()) * factor(i,k);
+      }
+    }
   }
-
-  return Spectrum(Spec_domain, SpectralRange(myres, units::inv_sr));
+} else {
+  if (! res.is_constant()) {
+    if (! stokes_coef->stokes_coefficient().is_constant()) {
+      Array<double, 2> stokes_coef_jac_sub(stokes_coef->stokes_coefficient().jacobian()
+        (Spec_index, Range(0, number_stokes() - 1), Range::all()));
+      res.jacobian() = sum((sum(stk.jacobian()(i1, i4, i2) * stokes_coef_sub(i4, i3), i4) +
+                            sum(stk.value()(i1, i4) * stokes_coef_jac_sub(i4, i3, i2), i4)) *
+                            factor(i1, i3), i3);
+    } else {
+      res.jacobian() = sum(sum(stk.jacobian()(i1, i4, i2) * stokes_coef_sub(i4, i3), i4) *
+                           factor(i1, i3), i3);
+    }
+  } else if (! stokes_coef->stokes_coefficient().is_constant()) {
+    Array<double, 2> stokes_coef_jac_sub(stokes_coef->stokes_coefficient().jacobian()
+      (Spec_index, Range(0, number_stokes() - 1), Range::all()));
+    res.jacobian() = sum(sum(stk.value()(i1, i4) * stokes_coef_jac_sub(i4, i3, i2), i4) *
+                         factor(i1, i3), i3);
+  }
+}
+  return Spectrum(Spec_domain, SpectralRange(res, units::inv_sr));
 }
